680cc64ac930acebac8a553eb54f2089df499a9f
Syohei YOSHIDA <syohex@gmail.com>
2012-02-06 10:08:49 +0900

    init repos

---
+++ lib/App/trigger.pm
@@ -0,0 +1,41 @@
+package App::trigger;
+
+use 5.008_001;
+use strict;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=for stopwords
+
+=head1 NAME
+
+App::trigger -
+
+=head1 SYNOPSIS
+
+  use App::trigger;
+
+=head1 DESCRIPTION
+
+App::trigger is
+
+=head1 AUTHOR
+
+Syohei YOSHIDA E<lt>syohex@gmail.comE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2012- Syohei YOSHIDA
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
